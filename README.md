0837b59d036276ed8756190a383a371a6d03d123
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-11-15 14:55:33 +0000

    incluido clases matriz dispersa y vector disperso

---
+++ README.md
@@ -0,0 +1,6 @@
+-Estructura de datos
+-Jerarquía de clases
+
+		      |1  1/2|   +  |0   0|
+		      |3  3/2]      |0   1|
+		  (matriz densa)    (matriz dispersa)
--- lib/matriz_sf.rb
+++ lib/matriz_sf.rb
@@ -1,5 +1,51 @@
 require "matriz_sf/version"
 
-module MatrizSf
-  # Your code goes here...
+class Vectordisperso
+  attr_reader_:vector
+  
+  def initialize(h = {})
+    @vector = Hash.new(0)
+    @vector = @vector.merge!(h)
+  end
+  
+  def [](j)
+    @vector[j]
+  end
+
+  def to_s
+    @vector.to_s
+  end
+end
+
+class Matrizdispersa
+  attr_reader_:matriz
+  
+  def initialize(h = {})
+    @matrix = Hash.new({})
+    for k in h.keys do 
+      @matrix[k] = if h[k].is_a? Vectordisperso
+                     h[k]
+                   else 
+                     @matrix[k] = Vectordisperso.new(h[k])
+                   end
+    end
+  end
+
+  def [](i)
+    @matrix[i]
+  end
+
+  def col(j)
+    c = {}
+    for r in @matrix.keys do
+      c[r] = @matrix[r].vector[j] if @matrix[r].vector.keys.include? j
+    end
+    Vectordisperso.new c
+  end
+  
+  
+  def to_s
+    str = @matriz.map {|k,v| "#{k} => #{v.to_s}"}.join(", ")
+    str = "{ #{str} }"
+  end
 end
